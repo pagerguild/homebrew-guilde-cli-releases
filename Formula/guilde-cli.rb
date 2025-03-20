69093e5845bc89943107714b74cd275e8d3b82f1
Tyler Gannon <tyler@pagerguild.com>
2025-03-20 20:34:15 +0000

    Release v0.41.19

--- Formula/guilde-cli.rb
+++ Formula/guilde-cli.rb
@@ -1,27 +1,27 @@
 class GuildeCli < Formula
   desc "Guilde Command Line Interface"
   homepage "https://github.com/pagerguild/homebrew-guilde-cli-releases"
-  version "0.41.12"
+  version "0.41.19"
 
   on_macos do
     on_arm do
-      url "https://guilde-cli-releases.vercel.app/guilde-cli-0.41.12-darwin-arm64.zip"
-      sha256 "44449cbf714b8e68f89031b2cd25b2b1ce7d4cce03352c270e4aac3335cbc8c2"
+      url "https://github.com/pagerguild/homebrew-guilde-cli-releases/releases/download/v0.41.19/guilde-cli-darwin-arm64.zip"
+      sha256 "780f7c8b5c265982380c6f15128f68aa983462e4fab251a983b1df140a618fb9"
     end
     on_intel do
-      url "https://guilde-cli-releases.vercel.app/guilde-cli-0.41.12-darwin-amd64.zip"
-      sha256 "07bb59cf12e12b23b5b6e7565cbba5971fd75471625b8f16e628d06ba8b7db31"
+      url "https://github.com/pagerguild/homebrew-guilde-cli-releases/releases/download/v0.41.19/guilde-cli-darwin-amd64.zip"
+      sha256 "d9bcca509e8d2cc6a3da199bc17d321d3bacff6a4ddc10bc0fdd57dc6d3375f9"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://guilde-cli-releases.vercel.app/guilde-cli-0.41.12-linux-arm64.zip"
-      sha256 "fbe86dd541a343ac7ecbbf1522df4ec66478c39f6d9dae4f25b395ee41a1a957"
+      url "https://github.com/pagerguild/homebrew-guilde-cli-releases/releases/download/v0.41.19/guilde-cli-linux-arm64.zip"
+      sha256 "ead63eaa3d6b693ead81689d61033999d2d49d35290cd485ab721ed5ccdeac02"
     end
     on_intel do
-      url "https://guilde-cli-releases.vercel.app/guilde-cli-0.41.12-linux-amd64.zip"
-      sha256 "fb9a79a7d8adc10f1efc2402e22e2d98eaf1601d966b0971cf4bfee03c670e75"
+      url "https://github.com/pagerguild/homebrew-guilde-cli-releases/releases/download/v0.41.19/guilde-cli-linux-amd64.zip"
+      sha256 "a89e7038213f2bff8264e3766fac32ed4b30e9b7b7783aa05bd2cfc22b0d892e"
     end
   end
 
@@ -29,3 +29,4 @@ class GuildeCli < Formula
     bin.install "guilde"
   end
 end
+
