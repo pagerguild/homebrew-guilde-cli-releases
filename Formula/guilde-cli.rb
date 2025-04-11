3c61d781bdb6bd9333c81ae0579ca5023c93121b
Tyler Gannon <tyler@pagerguild.com>
2025-04-11 21:21:59 +0000

    Release v0.43.3

--- Formula/guilde-cli.rb
+++ Formula/guilde-cli.rb
@@ -1,27 +1,27 @@
 class GuildeCli < Formula
   desc "Guilde Command Line Interface"
   homepage "https://github.com/pagerguild/homebrew-guilde-cli-releases"
-  version "0.43.2"
+  version "0.43.3"
 
   on_macos do
     on_arm do
-      url "https://github.com/pagerguild/homebrew-guilde-cli-releases/releases/download/v0.43.2/guilde-cli-darwin-arm64.zip"
-      sha256 "5a93386a84a236e00b21bb953db14386c8761980508a04c541eb8e5d7888fe2d"
+      url "https://github.com/pagerguild/homebrew-guilde-cli-releases/releases/download/v0.43.3/guilde-cli-darwin-arm64.zip"
+      sha256 "ad588c8bc291be59e7fe7506084146d5d1b78f016783876e4fba35217409573a"
     end
     on_intel do
-      url "https://github.com/pagerguild/homebrew-guilde-cli-releases/releases/download/v0.43.2/guilde-cli-darwin-amd64.zip"
-      sha256 "cb85d865677c1075947bf50bc5b58f183dd860f92217a34ba18ad422be7994d2"
+      url "https://github.com/pagerguild/homebrew-guilde-cli-releases/releases/download/v0.43.3/guilde-cli-darwin-amd64.zip"
+      sha256 "c02a2406bdbac615b1528868e3e0a61e5db7a8905be58dd4670212067347a72c"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/pagerguild/homebrew-guilde-cli-releases/releases/download/v0.43.2/guilde-cli-linux-arm64.zip"
-      sha256 "5369e05a547c4214121dc899cd8c4e0555b66358b66854ffef841e38b3903e90"
+      url "https://github.com/pagerguild/homebrew-guilde-cli-releases/releases/download/v0.43.3/guilde-cli-linux-arm64.zip"
+      sha256 "450beb54ba21de42e5eca9979faf72b1a4ba5176bcb02d989c1d93800a3024a2"
     end
     on_intel do
-      url "https://github.com/pagerguild/homebrew-guilde-cli-releases/releases/download/v0.43.2/guilde-cli-linux-amd64.zip"
-      sha256 "bc2f44af60f95895cccad0bf024e70699e655ad261780fafbd6c6b5ecf969acf"
+      url "https://github.com/pagerguild/homebrew-guilde-cli-releases/releases/download/v0.43.3/guilde-cli-linux-amd64.zip"
+      sha256 "69e03455c35ff91bde59dcf34f681184ea5ef1bb4a58083f0ff7ddce96065e08"
     end
   end
 
